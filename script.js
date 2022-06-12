fea56086a6548149a49d9e931511a66a5518033e
divijkhaitan <divijkhaitan@gmail.com>
2022-06-12 18:08:55 +0530

    Add logic for grid

---
+++ script.js
@@ -0,0 +1,22 @@
+const divcontainer =document.querySelector(".divcont")
+const divrows=document.createElement('div');
+divrows.classList.add('divrow')
+
+let temp;
+for(let i=0;i<16;i++)
+{
+    const divcols=document.createElement('div');
+    divcols.classList.add('divcol')
+    divcols.style.display="flex"
+    for(let i=0;i<16;i++)
+    {
+        const div1=document.createElement('div'); 
+        div1.style.border= "2px solid black"
+        div1.style.width="20px"
+        div1.style.height="20px"
+        divcols.appendChild(div1);
+    }
+    divrows.appendChild(divcols)
+}
+console.log(1)
+divcontainer.appendChild(divrows);
