0f6a88451927f925f89820fe9839768d6e2705a4
divijkhaitan <divijkhaitan@gmail.com>
2022-06-16 17:25:03 +0530

    Add form, padding between grid and top

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-const divcontainer =document.querySelector(".divcont")
-const divrows=document.createElement('div');
-divrows.classList.add('divrow')
-
-let temp;
-for(let i=0;i<16;i++)
+const size=640
+const divcontainer=document.querySelector(".divcont")    
+draw(16);
+const btn=document.querySelector('.btn')
+btn.addEventListener('click',clicked)
+function draw(boxnumber)
 {
-    const divcols=document.createElement('div');
-    divcols.classList.add('divcol')
-    divcols.style.display="flex"
-    for(let i=0;i<16;i++)
+    const divrows=document.createElement('div');
+    divrows.classList.add('divrow')
+    
+    while(divcontainer.firstChild)
+    divcontainer.removeChild(divcontainer.lastChild)
+    
+    for(let i=0;i<boxnumber;i++)
     {
-        const div1=document.createElement('div'); 
-        div1.style.border= "2px solid black"
-        div1.style.width="40px"
-        div1.style.height="40px"
-        divcols.appendChild(div1);
+        const divcols=document.createElement('div');
+        divcols.classList.add('divcol')
+        for(let i=0;i<boxnumber;i++)
+        {
+            const div1=document.createElement('div'); 
+            div1.style.border= "2px solid black"
+            div1.style.width=`${size/boxnumber}px`
+            div1.style.height=`${size/boxnumber}px`
+            divcols.appendChild(div1);
+        }
+        divrows.appendChild(divcols)
     }
-    divrows.appendChild(divcols)
+    divcontainer.appendChild(divrows);
 }
-console.log(1)
-divcontainer.appendChild(divrows);
+
+function clicked()
+{
+    const form=document.querySelector('form')
+}
